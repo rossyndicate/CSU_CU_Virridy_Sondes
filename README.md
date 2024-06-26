85bf98cea9ba14aaaeae9c9e5c211b8c6b35e8b8
SamStruthers <sam.struthers2000@gmail.com>
2024-06-26 09:14:02 -0600

    prep update for actual QAQC'ed data

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CSU_CU_Virridy_Sondes
-Map of sonde locations for CSU, CU and Virridy collaboration in the Cache La Poudre Basin
+
+This repo contains code to aggregate water quality sensor and grab sample data for the CSU, CU and Virridy collaboration in the Cache La Poudre Basin
+
+## Code
+
+All scripts are stored in the `scripts` folder. The primary script
--- scripts/00_setup.R
+++ scripts/00_setup.R
@@ -25,7 +25,7 @@ pack_req <- c(
 package_load(pack_req)
 
 #for API pull
-devtools::install_github("steeleb/HydroVuR")
+#devtools::install_github("steeleb/HydroVuR")
 library(HydroVuR)
 
 remove(pack_req, package_load)
@@ -34,13 +34,6 @@ remove(pack_req, package_load)
 
 ### ----- Meta Data ----- ###
 
-# Distance order and xdistance
-# order_table <- tibble(
-#   site_code = c('JOEI', 'JOER', 'CBRI', 'CBRR', 'CHD', 'BRNR', 'BMD', 'LNGR', 'LNGO', 'PTRR', 'PTRO', 'COMI', 'COMR', 'COMO', 'HORI', 'HORR', 'HORO', 'BEAV', 'JWC', 'PJW', 'PFAL', 'SLEP', 'PBR', 'SFM', 'PSF', 'PNF', 'PBD'),
-#   Distance = c('1 - JOEI', '2 - JOER', '3 - CBRI', '4 - CBRR', '5 - CHD', '6', '7', '8', '9', '99', '999', '11 - COMI', '12 - COMR', '13 - COMO', '14 - HORI', '15 - HORR', '16 - HORO', '17 - BEAV', '18 - JWC', '19 - PJW','20- SLEP', '20 - SLEP', '21 - PBR', '22 - SFM', '23 - PSF', '24 - PNF', '25 - PBD'),
-#   Xdistance = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 99, 999, 12, 13, 14, 15, 16, 17, 18, 19,20, 20, 21, 22, 23, 24, 25, 26),
-#   order = c('03', '04', '05', '06', '07', '10', '11', '01', '02', '08', '09', '12', '13', '14', '15', '16', '17', '18', '19','20', '20', '21', '22', '23', '24', '25', '26')
-# )
 # df for sensor metadata
 sensor_meta <- tibble(
   # param sonde is the name that the sensor records
@@ -64,70 +57,5 @@ sensor_meta <- tibble(
                                     F,T,T,T,T,F,
                                     T,T,F))
 
-### ----- site and date groupings ----- ###
-# 
-# #Dates for genomic survey
-# genomic_dates <- as.Date(c("2022-06-22", "2022-08-25", "2022-09-19"))
-# #JW and Chambers complex
-# jw_chambers_complex <- c("JOEI", "JOER", "CBRR", "CBRI", "CHD")
-# 
-# #Sonde sites 2023
-# sonde_sites <- c("JOEI", "CBRI", "CHD", "PBD", "SFM", "lincoln", "timberline", "prospect", "boxcreek", "boxelder", "archery")
-# 
-# #Weekly reservoir study sites
-# mainstem_res_set <- c("CBRR", "CHD", "JOEI", "JOER", "CBRI", "PTRR", "PTRO", "BMD", "BRNR", "LNGO", "LNGR")
-# 
-# #^^ but only reservoirs
-# mainstem_Res_only <-c("CBRR", "JOER", "PTRR",  "BRNR", "LNGR")
-# 
-# #southfork reservoirs
-# SF_Res <- c("COMI", "COMR", "COMO", "HORI","HORR" , "HORO", "BEAV", "SFM")
-# 
-# #study mainstem sites
-# mainstem_sites <- c("JWC","PJW","SLEP","PBR","SFM","PSF","PNF","PBD")
-# 
-# #overlap sites between fc gov and our study
-# fcgov_study <- c("CHD", "BMD","JWC", "PJW", "PBR", "SFM", "PSF", "PNF", "PBD")
-# #SF and Weekly study reservoirs
-# all_res_system <- c("COMI", "COMR", "COMO", "HORI","HORR" , "HORO","CBRR", "CHD", "JOEI", "JOER", "CBRI", "PTRR", "PTRO", "BMD", "BRNR", "LNGO", "LNGR")
-# 
-# genomic_sites <- c("LEGC", "TMBR", "TIMB", "ELC", "ARCH", "BOX1", "BOX2", "BOX3", "BOX4")
-# 
-# # Isco rmrs cpf sites
-# ISCO <- c("FISH_ISCO", "SAWM_ISCO", "BLAK_ISCO", "LBEA_ISCO", "FALL_ISCO")
-# 
-# # Ratio Sites for spatial mapping
-# ratio_sites_names_all <- c("JOEI_JOER", "JOER_CBRI" , "CBRI_CBRR" , "CBRR_CHD" , "CHD_JWC" , "BRNR_BMD" , "BMD_JWC" , "PTRR_PTRO" , 
-#                        "PTRO_PJW" , "LNGR_LNGO" , "LNGO_PJW" , "JWC_SLEP" ,"PJW_SLEP" , "SLEP_PBR" , "PBR_PSF" , "COMI_COMR" , 
-#                        "COMR_COMO" , "COMO_BEAV"  , "HORI_HORR" , "HORR_HORO" , "HORO_BEAV" , "BEAV_SFM", "SFM_PSF", "PSF_PNF" , "PNF_PBD" )
-# # Ratio upper canyon Sites for spatial mapping
-# ratio_sites_names_upper <- c("JOEI_JOER", "JOER_CBRI" , "CBRI_CBRR" , "CBRR_CHD" , "CHD_JWC" , "BRNR_BMD" , "BMD_JWC" , "PTRR_PTRO" , 
-#                            "PTRO_PJW" , "LNGR_LNGO" , "LNGO_PJW" , "JWC_SLEP" ,"PJW_SLEP"  )
-# # for upper select graph
-# 
-# ratio_sites_names_upper_select <- c("JOEI_JOER", "JOER_CBRI" , "CBRI_CBRR" , "CBRR_CHD" , "CHD_JWC" , "BRNR_BMD" , "BMD_JWC"  , 
-#                                       "JWC_SLEP" ,"PJW_SLEP"  )
-# 
-# sites_names_upper_select <- c("JOEI", "JOER", "CBRI", "CBRR", "CHD", "BRNR", "BMD", "JWC", "PJW", "SLEP")
-# 
-# ratio_sites_names_SF <- c("PBR_PSF" , "COMI_COMR" , "COMR_COMO" , "COMO_BEAV" ,
-#                          "HORI_HORR" , "HORR_HORO" , "HORO_BEAV" , "BEAV_SFM", "SFM_PSF")
-# 
-# ### ----- Color Sets ----- ###
-# 
-# cbbPalette <- c( "#999999","#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-# 
-# #Color for watersheds, matches burn severity
-# colorsBS <- c("Barnes Meadow Reservoir" = "#D55E00", "Peterson Reservoir" ="#E69F00", "Chambers Reservoir" = "#F0E442", "Joe Wright Reservoir" = "#56B4E9", "Long Draw Reservoir" = "#0072B2")
-# 
-# #Color for reservoirs, matches burn severity by site code
-# 
-# colorsBS_site_code <- c("BRNR" = "#D55E00", "CBRR" = "#F0E442", "JOER" = "#56B4E9", "LNGR" = "#0072B2", "PTRR" ="#E69F00" )
-
-#Color matches burn severity
-#colorsBS_in_order <-c("#0072B2","#56B4E9","#F0E442","#E69F00","#D55E00")
-
-#Seasonal Color values
-#season_color_vals =c('#047E82','#397534','#59B851','#DEB907','#FA850E')
 
 
